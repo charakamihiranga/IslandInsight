24d28795313874fc22938c56430fb7c75b611386
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-09-07 18:57:08 +0530

    scrape news : create endpoint to get all news & get news content

--- IslandInsight-backend/app.py
+++ IslandInsight-backend/app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask,jsonify
+import Scrape
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 app = Flask(__name__)
 
+@app.route('/gettrendingnews')
+def getTrendingNews():
+    news = Scrape.scrapeTrendingNews()
+    return jsonify(news)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+@app.route('/getnewsdetails', defaults={'newsId': 0})
+@app.route('/getnewsdetails/<int:newsId>')
+def getNewsDetails(newsId):
+    # Construct the URL for the specific news article
+    news_url = f'https://UrlOftheNewsWebsite/news/{newsId}'
+    try:
+        # Fetch the news details
+        news_details = Scrape.scrapeNewsDetails(news_url)
+        return jsonify(news_details)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # Return an error message if something goes wrong
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
