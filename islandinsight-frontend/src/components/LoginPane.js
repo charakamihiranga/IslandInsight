94ad2f1bbb1007fc0df89f3eac4b7c8f6122b33e
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-09-17 21:47:29 +0530

    islandInsight-frontend - login popup frontend implementation

---
+++ islandinsight-frontend/src/components/LoginPane.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import "./../styles/general.css";
+import closeIcon from "./../assets/Close.png";
+import eyeIcon from "./../assets/eye.png";
+
+const LoginPane = ({ isOpen, onClose }) => {
+  const [closing, setClosing] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const handleMouseDown = () => {
+    setPasswordVisible(true);
+  };
+
+  const handleMouseUp = () => {
+    setPasswordVisible(false);
+  };
+
+  useEffect(() => {
+    if (!isOpen) setClosing(false);
+  }, [isOpen]);
+
+  const handleClose = () => {
+    setClosing(true);
+    setTimeout(() => {
+      onClose();
+      setClosing(false);
+    }, 300);
+  };
+
+  if (!isOpen && !closing) return null;
+
+  return (
+    <div
+      className={`fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 transition-opacity duration-300 ease-in-out ${
+        isOpen && !closing
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <div
+        className={`bg-white p-8 rounded-2xl shadow-lg relative transition-transform duration-300 ease-in-out ${
+          isOpen && !closing ? "zoom-in" : "zoom-out"
+        }`}
+      >
+        <button
+          className="absolute top-4 right-5 text-gray-500 hover:text-gray-800 transition-transform transform hover:scale-110"
+          onClick={handleClose}
+        >
+          <img src={closeIcon} alt="Close" className="h-6 w-6 fill-current" />
+        </button>
+
+        <div className="flex flex-col items-center cursor-pointer ">
+          <h1 className="text-3xl text-center font-Pacifico text-[#E51B21] ">
+            Island Insight
+          </h1>
+          <div className="flex items-center mt-2">
+            <div className="w-24 h-0.5 bg-[#E51B21]"></div>
+            <div className="ml-4 font-roboto font-bold text-xs text-gray-700">
+              Login form
+            </div>
+          </div>
+          <div>
+            <h3 className="roboto-bold mt-8">
+              Your fastest, most trusted source for News
+            </h3>
+            <p className="roboto-bold text-xs text-gray-600">
+              Login to stay ahead
+            </p>
+          </div>
+        </div>
+        <div className="display-block mt-8 ">
+          <p className="text-xs roboto-bold mt-2 text-gray-700 ml-10">
+            Email or phone number
+          </p>
+          <input className="w-[96%] h-10 mt-4 ml-3 rounded-3xl border-none bg-[#d9d9d9] pl-4" />
+          <p className="text-xs mt-4 roboto-bold text-gray-700 ml-10">
+            Password
+          </p>
+          <div className="relative w-[96%] mt-2 ml-3">
+            <input
+              className="w-full h-10 rounded-3xl border-none bg-[#d9d9d9] pl-4 pr-10"
+              type={passwordVisible ? "text" : "password"}
+            />
+            <img
+              src={eyeIcon}
+              alt="Toggle visibility"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer h-4 mr-2"
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+            />
+          </div>
+          <div className="flex justify-end">
+            <p className="text-[10px] roboto-medium text-gray-700 cursor-pointer mt-2 hover:underline mr-6">
+              Forgot password?
+            </p>
+          </div>
+          <button className="w-full bg-[#E51B21] text-white roboto-bold text-xs p-2.5 rounded-3xl mt-4 cursor-pointer hover:bg-[#d91c27] hover:shadow-lg transition duration-300 ease-in-out">
+            LOGIN
+          </button>
+          <div className="mt-6 flex">
+            <div className="w-[8vw] mt-2 h-[1.5px] bg-[#716868]"></div>
+            <p className="roboto-bold text-xs text-gray-700 mx-3">
+              {" "}
+              To create an account
+            </p>
+            <div className="w-[8vw] mt-2 h-[1.5px] bg-[#716868]"></div>
+          </div>
+          <button className="w-full bg-[#D9D9D9] text-[#000] mb-4 roboto-bold text-xs p-2.5 rounded-3xl mt-4 cursor-pointer hover:bg-[#B0B0B0] hover:shadow-lg transition duration-300 ease-in-out">
+            SIGNUP
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPane;
--- islandinsight-frontend/src/components/Navbar.js
+++ islandinsight-frontend/src/components/Navbar.js
@@ -1,77 +1,88 @@
 import React, { useState } from 'react';
 import './../styles/general.css';
+import LoginPane from './LoginPane';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showLoginPane, setShowLoginPane] = useState(false); 
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const toggleLoginPane = () => {
+    setShowLoginPane(!showLoginPane); 
+  };
+
   return (
-    <nav className="bg-white border-b border-gray-200 py-4 md:py-6 sticky top-0 left-0 z-50 shadow-md">
-      <div className="container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8 xl:px-[195px] relative">
-        <a href="#" className="text-red-600 font-Pacifico text-2xl">
-          Island Insight
-        </a>
-        <div className="hidden md:flex border border-[#B4A8A8] h-5 ml-6"></div>
-        <div className="hidden md:flex space-x-12 ml-6 justify-start flex-grow lg:ml-12 xl:ml-[56px]">
-          <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Latest</a>
-          <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Business</a>
-          <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Technology</a>
-          <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Sport</a>
-        </div>
-        <div className="flex space-x-4">
-          <button
-            className="hidden md:block bg-black text-white text-xs roboto-bold py-2 px-4 rounded-lg hover:bg-gray-800 transition-colors duration-300"
-            onClick={() => alert('Login')}
-          >
-            LOGIN
-          </button>
-          <button
-            className={`md:hidden absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-700 hover:text-red-600 transition-transform duration-300 ${isOpen ? 'rotate-45' : ''}`}
-            onClick={toggleMenu}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'} bg-white border-t border-gray-200 transition-transform duration-500 ease-in-out transform ${isOpen ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'} w-full`}>
-        <div className="flex flex-col space-y-4 py-5 px-4 sm:px-6 lg:px-8">
-          <a href="#" className="text-gray-700 hover:text-red-600">Latest</a>
-          <a href="#" className="text-gray-700 hover:text-red-600">Business</a>
-          <a href="#" className="text-gray-700 hover:text-red-600">Technology</a>
-          <a href="#" className="text-gray-700 hover:text-red-600">Sport</a>
-          <div className="flex flex-col space-y-4">
+    <>
+      <nav className="bg-white border-b border-gray-200 py-4 md:py-6 sticky top-0 left-0 z-50 shadow-md">
+        <div className="container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8 xl:px-[195px] relative">
+          <a href="#" className="text-red-600 font-Pacifico text-2xl">
+            Island Insight
+          </a>
+          <div className="hidden md:flex border border-[#B4A8A8] h-5 ml-6"></div>
+          <div className="hidden md:flex space-x-12 ml-6 justify-start flex-grow lg:ml-12 xl:ml-[56px]">
+            <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Latest</a>
+            <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Business</a>
+            <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Technology</a>
+            <a href="#" className="text-gray-700 hover:text-red-600 roboto-medium">Sport</a>
+          
+          </div>
+          <div className="flex space-x-4">
             <button
-              className="bg-black text-white text-xs roboto-bold py-2 px-4 rounded-lg hover:bg-gray-800 transition-colors duration-300"
-              onClick={() => alert('Login')}
+              className="hidden md:block bg-black text-white text-xs roboto-bold py-2 px-4 rounded-lg hover:bg-gray-800 transition-colors duration-300"
+              onClick={toggleLoginPane} 
             >
               LOGIN
             </button>
             <button
-              className="bg-gray-300 text-black text-xs roboto-bold py-2 px-4 rounded-lg hover:bg-gray-400 transition-colors duration-300"
-              onClick={() => alert('Signup')}
+              className={`md:hidden absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-700 hover:text-red-600 transition-transform duration-300 ${isOpen ? 'rotate-45' : ''}`}
+              onClick={toggleMenu}
             >
-              SIGNUP
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+                />
+              </svg>
             </button>
           </div>
         </div>
-      </div>
-    </nav>
+        <div className={`md:hidden ${isOpen ? 'block' : 'hidden'} bg-white border-t border-gray-200 transition-transform duration-500 ease-in-out transform ${isOpen ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'} w-full`}>
+          <div className="flex flex-col space-y-4 py-5 px-4 sm:px-6 lg:px-8">
+            <a href="#" className="text-gray-700 hover:text-red-600">Latest</a>
+            <a href="#" className="text-gray-700 hover:text-red-600">Business</a>
+            <a href="#" className="text-gray-700 hover:text-red-600">Technology</a>
+            <a href="#" className="text-gray-700 hover:text-red-600">Sport</a>
+            <div className="flex flex-col space-y-4">
+              <button
+                className="bg-black text-white text-xs roboto-bold py-2 px-4 rounded-lg hover:bg-gray-800 transition-colors duration-300"
+                onClick={toggleLoginPane} 
+              >
+                LOGIN
+              </button>
+              <button
+                className="bg-gray-300 text-black text-xs roboto-bold py-2 px-4 rounded-lg hover:bg-gray-400 transition-colors duration-300"
+                onClick={() => alert('Signup')}
+              >
+                SIGNUP
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <LoginPane isOpen={showLoginPane} onClose={toggleLoginPane} />
+    </>
   );
 };
 
